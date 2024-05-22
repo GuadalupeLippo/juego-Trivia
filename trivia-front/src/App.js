db341469159acc0b4c3e2b082ecb6e11d21b9f2f
AilenMassa <ailenmassa1@gmail.com>
2024-05-22 20:25:56 -0300

    carpetas css

--- trivia-front/src/App.js
+++ trivia-front/src/App.js
@@ -1,14 +1,11 @@
 
 import './App.css';
+import { Home } from './pages/home';
 function App() {
-  return (
-    <div className="App">
-          <div className="container">
-      <h1 className="text-center mt-5">¡Hola, Bootstrap con React!</h1>
-      <button className="btn btn-primary">Botón de Bootstrap</button>
-    </div>
-    </div>
-  );
+  return (<div>
+  <Home/>
+  </div>)
 }
+
 
 export default App;
--- trivia-front/src/pages/home.js
+++ trivia-front/src/pages/home.js
@@ -1,2 +1,8 @@
+import Welcome from "../components/welcome";
+import Rules from "../components/rules";
 
-
+export function Home (){
+ return (
+ <div><Welcome/>
+<Rules/></div>)
+}
