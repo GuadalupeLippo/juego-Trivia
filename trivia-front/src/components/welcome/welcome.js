fc33ffa20c34797000bd853a31ed3d73f37ad152
AilenMassa <ailenmassa1@gmail.com>
2024-05-27 17:15:02 -0300

    actualizacion welcome

--- trivia-front/src/components/welcome/welcome.js
+++ trivia-front/src/components/welcome/welcome.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import './welcome.css';
+import EfectoDesencriptarTexto from './effect';
 
 export default function Welcome() {
+    
+    const TextoTitulo= "¡BIENVENIDO A ATRIVIATE!"
+      
   return (
-    <div class= "background">
-    <div class="container2">
-      <p class="title2">¡Bienvenidos a Atriviate!</p>
-      <p class="title3">Corre contra el reloj y pon a prueba tus conocimientos con este dinámico y educativo juego de preguntas y respuestas.</p>
-      <p class="title2">¡Gracias por jugar con nosotros!</p>
-    </div>
-    </div>
-  )
+    <div className="container2">
+          <div className="title1">
+            <EfectoDesencriptarTexto 
+            text= 
+            {TextoTitulo}
+            mLeftInicial={700}
+            transitionTime={5}
+            tickCambioLetra={120}
+            />
+            </div>
+            
+      <p className="title3">Corre contra el reloj y pon a prueba tus conocimientos con este dinámico y educativo juego de preguntas y respuestas.</p>
+      <p className="title2">¡Gracias por jugar con nosotros!</p>
+   </div>
+  
+     )
 }
