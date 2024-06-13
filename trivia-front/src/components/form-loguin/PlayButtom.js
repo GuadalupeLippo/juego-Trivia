2041edaf8d232a2f2e7cf1124ac9e8a65967295d
Guadalupe Lippo <gualippo@gmail.com>
2024-06-12 21:30:32 -0300

    cambios de color en css

--- trivia-front/src/components/form-loguin/PlayButtom.js
+++ trivia-front/src/components/form-loguin/PlayButtom.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
+import './PlayButtom.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function PlayButton() {
   const loguin = useNavigate();
@@ -13,8 +15,8 @@ function PlayButton() {
 
 
   return (
-    <button className="btn btn-primary btn-lg" onClick={handleNavigate}>
-      <FontAwesomeIcon icon={faPlay} /> Jugar
+    <button className="btn btn-grad btn-lg" onClick={handleNavigate}>
+      <FontAwesomeIcon icon={faPlay} />
     </button>
   );
 }
