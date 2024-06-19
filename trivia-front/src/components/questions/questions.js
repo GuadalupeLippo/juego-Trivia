2f4edcbc2b27f32fc5b32f09ddb38e8b69a932f6
AilenMassa <ailenmassa1@gmail.com>
2024-06-18 21:12:54 -0300

    agregando pregunta div

---
+++ trivia-front/src/components/questions/questions.js
@@ -0,0 +1,11 @@
+import './questions.css'
+
+export function Question (){
+    return (
+        <div className='questionContainer'>
+            <div className='question'>
+                <p>PREGUNTA</p>
+            </div>
+        </div>
+    )
+}
