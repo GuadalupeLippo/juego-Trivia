49b59bb9f4d741ee6c38e37d44ca66fb559a97a5
AilenMassa <ailenmassa1@gmail.com>
2024-06-18 21:57:33 -0300

    agregando respuestas

---
+++ trivia-front/src/components/answers/answers.js
@@ -0,0 +1,12 @@
+import './answers.css';
+
+export function Answers (){
+    return (
+    <div className='answersContainer'>
+       <div className='answers'><p>RESPUESTA 1</p></div> 
+       <div className='answers'><p>RESPUESTA 2</p></div> 
+       <div className='answers'><p>RESPUESTA 3</p></div> 
+       <div className='answers'><p>RESPUESTA 4</p></div> 
+    </div>)
+    
+}
