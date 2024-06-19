a7e78f28122ded1eef8add8a789a802964c74cc3
Guadalupe Lippo <gualippo@gmail.com>
2024-06-19 20:37:12 -0300

    cambios nuevos

--- trivia-front/src/App.js
+++ trivia-front/src/App.js
@@ -4,12 +4,8 @@ import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import {Loguin} from '../src/pages/loguin.js'
 import Trivia from './pages/trivia.js';
 import PoliticasDePrivacidad from './components/Politicas/PoliticasDePrivacidad.js';
-<<<<<<< HEAD
-import { Trivia } from './pages/trivia.js';
-=======
 import User from './pages/user.js';
 import Store from './pages/store.js';
->>>>>>> 37890fe85ab4fcf2a2a60a64c92ce381a5ddb012
 <link href="https://fonts.googleapis.com/css2?family=Itim&display=swap" rel="stylesheet"></link>
 
 
