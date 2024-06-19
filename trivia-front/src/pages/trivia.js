d01244cd5b4b2d32387562ae058b897f64fa498a
Guadalupe Lippo <gualippo@gmail.com>
2024-06-19 20:38:13 -0300

    cambio de lugar barra

--- trivia-front/src/pages/trivia.js
+++ trivia-front/src/pages/trivia.js
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 import InicioTrivia from "../components/trivia/inicioTrivia";
-=======
 import React from 'react'
 import { Question } from '../components/questions/questions';
 import { Answers } from '../components/answers/answers';
@@ -10,16 +8,14 @@ export default function Trivia() {
   return (
     <div>
     <NavLoguin/>
+    <InicioTrivia />
     <Question/>
     <Answers/>
     </div>
   )
 }
->>>>>>> 37890fe85ab4fcf2a2a60a64c92ce381a5ddb012
 
 
-export function Trivia (){
-    return (
-        <InicioTrivia />
-    )
-}
+
+        
+ 
