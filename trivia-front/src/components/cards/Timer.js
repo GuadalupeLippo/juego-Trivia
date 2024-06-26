2487b098685be14fb61caeffda91348e61ef3b20
Yessica <yessilpd91@outlook.com>
2024-06-26 17:09:04 -0300

    sonido

--- trivia-front/src/components/cards/Timer.js
+++ trivia-front/src/components/cards/Timer.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './Timer.css';
+
+
+
 const Timer = ({ seconds }) => {
   const [timeLeft, setTimeLeft] = useState(seconds);
 
@@ -7,9 +10,10 @@ const Timer = ({ seconds }) => {
     if (timeLeft > 0) {
       const timerId = setTimeout(() => setTimeLeft(timeLeft - 1), 1000);
       return () => clearTimeout(timerId);
-    }
+    } 
   }, [timeLeft]);
 
+   
   return (
     <div className='timer'>
       <h1><strong> {timeLeft}</strong></h1>
--- trivia-front/src/components/trivia/inicioTrivia.js
+++ trivia-front/src/components/trivia/inicioTrivia.js
@@ -1,25 +1,32 @@
-import Reloj from "./reloj"
-import geografia from './geografia 1.png';
+import Reloj from "./reloj";
+import geografia from "./geografia 1.png";
 import { Question } from "../questions/questions";
-import './trivia.css'
+import "./trivia.css";
 import Timer from "../cards/Timer";
 import { useLocation } from "react-router-dom";
 
-function InicioTrivia (){
-    const location = useLocation();
+
+function InicioTrivia() {
+  const location = useLocation();
   const selectedTime = location.state?.selectedTime || 15;
-    return(
-        <>
-        <div className="inicioTrivia">
-          <Reloj /> 
-          
-      <Timer seconds={selectedTime} />
-     
-      
-        <Question/>   
-       <img alt='logoGeografia' src={geografia} className="geografia" width='100px'/>
-        </div>
-        </>
-    )
+  
+
+  return (
+    <>
+      <div className="inicioTrivia">
+        <Reloj />
+
+        <Timer seconds={selectedTime} />
+
+        <Question />
+        <img
+          alt="logoGeografia"
+          src={geografia}
+          className="geografia"
+          width="100px"
+        />
+      </div>
+    </>
+  );
 }
-export default InicioTrivia
+export default InicioTrivia;
