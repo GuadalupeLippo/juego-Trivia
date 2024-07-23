be3ecaf90a3dc600dcd74a890bd96df305364bf0
Guadalupe Lippo <gualippo@gmail.com>
2024-07-23 17:43:05 -0300

    ingreso de usuario y autenticacion

--- trivia-front/src/components/DropDownProfile/Dropdownprofile.js
+++ trivia-front/src/components/DropDownProfile/Dropdownprofile.js
@@ -3,10 +3,23 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
 import { ModalGames } from "./Modal-games";
 import { useState } from "react";
+import EditProfileModal from "./ModalEditProfile";
+import { useAuth } from "../form-loguin/UserAuth";
 import "./DropDownProfile.css";
 
 function DropDownProfiles() {
   const [openGames, setOpenGames] = useState(false);
+  const [showEditProfile, setShowEditProfile] = useState(false);
+  const { authUser, setAuthUser } = useAuth();
+
+  const handleEditProfileClick = () => setShowEditProfile(true);
+  const handleCloseEditProfile = () => setShowEditProfile(false);
+
+  const handleSaveUser = (user) => {
+    setAuthUser(user);
+    console.log('Datos guardados:', user);
+  };
+
 
   const handleOpenGames = () => setOpenGames(true);
   const handleCloseGames = () => setOpenGames(false);
@@ -18,16 +31,27 @@ function DropDownProfiles() {
       </Dropdown.Toggle>
 
       <Dropdown.Menu className="dropdown-menu-custom">
-        <Dropdown.Item href="#/action-3">
-          Mis puntos:<strong className="puntos">xxx</strong>
-        </Dropdown.Item>
-        <Dropdown.Item href="#/action-1">Editar perfil</Dropdown.Item>
+        <Dropdown.Header className="name-user">
+          <strong>{authUser?.name}</strong>
+        </Dropdown.Header>
+        <Dropdown.Header>
+          Puntuación: <strong className="puntos">{authUser?.score}</strong>
+        </Dropdown.Header>
+        <Dropdown.Item  onClick={handleEditProfileClick}>Editar perfil</Dropdown.Item>
         <Dropdown.Item href="/avatars">Mis avatars</Dropdown.Item>
         <Dropdown.Item onClick={handleOpenGames}>Partidas</Dropdown.Item>
 
         <Dropdown.Item href="/">Cerrar sesión</Dropdown.Item>
       </Dropdown.Menu>
       <ModalGames openGames={openGames} handleCloseGames={handleCloseGames} />
+      {authUser && (
+        <EditProfileModal
+          show={showEditProfile}
+          handleClose={handleCloseEditProfile}
+          user={authUser}
+          onSave={handleSaveUser}
+        />
+      )}
     </Dropdown>
   );
 }
