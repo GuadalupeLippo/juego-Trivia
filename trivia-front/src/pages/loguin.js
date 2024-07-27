e463d7d7003851a9a51a3d094393befb21c6a8ea
Guadalupe Lippo <gualippo@gmail.com>
2024-07-27 19:13:23 -0300

    footer y reseñas

--- trivia-front/src/pages/loguin.js
+++ trivia-front/src/pages/loguin.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Cards from "../components/cards/cards";
 import { NavLoguin } from "../components/nav-home/NavLoguin";
+import FooterLoguin from "../components/footer/FooterLoguin";
 
 export function Loguin() {
   return (
     <div>
       <NavLoguin />
       <Cards />
+      <FooterLoguin />
     </div>
   );
 }
