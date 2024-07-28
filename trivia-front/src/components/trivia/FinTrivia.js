8289a0c7008edad440c5cf5e3bdd8cd05ac8956c
AilenMassa <ailenmassa1@gmail.com>
2024-07-28 13:32:31 -0300

    ventana modal final de trivia

--- trivia-front/src/components/trivia/FinTrivia.js
+++ trivia-front/src/components/trivia/FinTrivia.js
@@ -4,8 +4,11 @@ import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { useNavigate } from "react-router-dom";
 import Logo from "../images/logocerebritotrivia.png";
+import circle from "./images/circle2.jpg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleCheck, faCircleXmark, faStar } from '@fortawesome/free-solid-svg-icons';
 
-function FinTrivia() {
+export function FinTrivia() {
   const [show, setShow] = useState(false);
   const loguin = useNavigate();
   const store = useNavigate();
@@ -16,6 +19,7 @@ function FinTrivia() {
   const handleNavigateStore = () => {
     store("/Store");
   };
+
   return (
     <>
       <Button variant="primary" onClick={() => setShow(true)}>
@@ -28,28 +32,45 @@ function FinTrivia() {
         dialogClassName="modal-90w"
         aria-labelledby="example-custom-modal-styling-title"
       >
-        <Modal.Header closeButton>
-          <div class="area">
-            ¡¡¡Felicitaciones!!!
-            <Modal.Title id="tituloFin"></Modal.Title>
+        <Modal.Header closeButton className="top_modal">
+          <div className="title_modal">
+            <h3 className="title_shine"> ¡¡¡Felicitaciones!!!</h3>
           </div>
         </Modal.Header>
         <Modal.Body>
-          <h1>Ganaste la partida de Trivia</h1>
-          <h2>Respuestas correctas:</h2>
-          <h2>Respuestas Incorrectas:</h2>
-          <h3>Puntaje total obtenido: </h3>
-          <img className="logoNav" src={Logo} alt="logo pagina" width="180" />
+       <h3 className="winner">Ganaste la partida</h3>
+          <div className="all_results"> 
+            <div className="result_container">
+              <img className="circle" src={circle} alt="circulo"/>
+              <FontAwesomeIcon icon="fa-duotone fa-solid fa-circle-check" />
+              <h1 className="trivia_results"> Correctas <FontAwesomeIcon className="correctas" icon={faCircleCheck}/> </h1>
+            </div>
+            <div className="result_container">
+              <img className="circle" src={circle} alt="circulo"/>
+              <h1 className="trivia_results">Incorrectas <FontAwesomeIcon className="incorrectas" icon={faCircleXmark} /></h1>
+            </div>
+            <div className="result_container">
+              <img className="circle" src={circle} alt="circulo"/>
+              <h1 className="trivia_results">Puntos <FontAwesomeIcon className="puntos" icon={faStar} /></h1>
+            </div>
+          </div>
+         
         </Modal.Body>
-        <Button variant="secondary" onClick={handleNavigate}>
-          Volver Jugar!
+        <img className="logoNav" src={Logo} alt="logo pagina" width="200" />
+
+        <div className="principal_button">
+        <Button className="btn1" variant="secondary" onClick={handleNavigate}>
+          Volver a Jugar!
         </Button>
-        <Button variant="primary" onClick={handleNavigate}>
+        </div>
+        <div className="other_buttons">
+        <Button className="btn2" variant="primary" onClick={handleNavigate}>
           Otra categoria
         </Button>
-        <Button variant="primary" onClick={handleNavigateStore}>
+        <Button className="btn2" variant="primary" onClick={handleNavigateStore}>
           Canjear puntos
         </Button>
+        </div>
       </Modal>
     </>
   );
